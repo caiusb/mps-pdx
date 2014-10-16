f8b1fc64d1e647e9e249ca332fc5c6ddabbeac6d
Mihai Codoban <codobanmihai@gmail.com>
2014-10-15 21:34:53 -0700

    Java 8 streams solution

--- src/blender/solutions/BlenderParallel.java
+++ src/blender/solutions/BlenderParallel.java
@@ -2,11 +2,9 @@ package blender.solutions;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.MemoryImageSource;
+import java.util.stream.IntStream;
 
 import blender.Blender;
-import extra166y.Ops;
-import extra166y.ParallelArray;
-import extra166y.ParallelLongArray;
 
 public class BlenderParallel extends Blender {
 
@@ -17,39 +15,35 @@ public class BlenderParallel extends Blender {
 
 	@Override
 	public void process() {
-		ParallelLongArray pa = ParallelLongArray.create(height,
-				ParallelArray.defaultExecutor());
+		IntStream.range(0, height).parallel().forEach(row -> combineRows(row));
+	}
 
-		pa.replaceWithMappedIndex(new Ops.IntToLong() {
-			@Override
-			public long op(int row) {
-				int[] rgbim1 = new int[width];
-				int[] rgbim2 = new int[width];
+	private void combineRows(int row) {
+		int[] rgbim1 = new int[width];
+		int[] rgbim2 = new int[width];
 
-				img1.getRGB(0, row, width, 1, rgbim1, 0, width);
-				img2.getRGB(0, row, width, 1, rgbim2, 0, width);
+		img1.getRGB(0, row, width, 1, rgbim1, 0, width);
+		img2.getRGB(0, row, width, 1, rgbim2, 0, width);
 
-				for (int col = 0; col < width; col++) {
-					int rgb1 = rgbim1[col];
-					int r1 = (rgb1 >> 16) & 255;
-					int g1 = (rgb1 >> 8) & 255;
-					int b1 = rgb1 & 255;
+		for (int col = 0; col < width; col++) {
+			int rgb1 = rgbim1[col];
+			int r1 = (rgb1 >> 16) & 255;
+			int g1 = (rgb1 >> 8) & 255;
+			int b1 = rgb1 & 255;
 
-					int rgb2 = rgbim2[col];
-					int r2 = (rgb2 >> 16) & 255;
-					int g2 = (rgb2 >> 8) & 255;
-					int b2 = rgb2 & 255;
+			int rgb2 = rgbim2[col];
+			int r2 = (rgb2 >> 16) & 255;
+			int g2 = (rgb2 >> 8) & 255;
+			int b2 = rgb2 & 255;
 
-					int r3 = (int) (r1 * weight + r2 * (1.0 - weight));
-					int g3 = (int) (g1 * weight + g2 * (1.0 - weight));
-					int b3 = (int) (b1 * weight + b2 * (1.0 - weight));
+			int r3 = (int) (r1 * weight + r2 * (1.0 - weight));
+			int g3 = (int) (g1 * weight + g2 * (1.0 - weight));
+			int b3 = (int) (b1 * weight + b2 * (1.0 - weight));
 
-					imageBuffer[row * width + col] = new java.awt.Color(r3, g3,
-							b3).getRGB();
-				}
-				imageSource.newPixels(0, row, width, 1, true);
-				return 10;
-			}
-		});
+			imageBuffer[row * width + col] = new java.awt.Color(r3, g3, b3)
+					.getRGB();
+		}
+
+		imageSource.newPixels(0, row, width, 1, true);
 	}
 }
--- src/primes/solutions/PrimesParallel.java
+++ src/primes/solutions/PrimesParallel.java
@@ -1,19 +1,25 @@
 package primes.solutions;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 import primes.PrimesComputation;
-import extra166y.Ops.IntToObject;
-import extra166y.ParallelArray;
 
 public class PrimesParallel extends PrimesComputation {
+
 	@Override
 	public Boolean[] computePrimes(int upto) {
-		ParallelArray<Boolean> p = ParallelArray.create(upto, Boolean.class, ParallelArray.defaultExecutor());
-		p.replaceWithMappedIndex(new IntToObject<Boolean>() {
-			@Override
-			public Boolean op(int x) {
-				return isPrime(x);
-			}
-		});			
-		return p.getArray();
+		Boolean[] results = new Boolean[upto];
+		
+		List<Boolean> booleanList  = IntStream.range(0, upto)
+										.parallel()
+										.mapToObj(PrimesComputation::isPrime)
+										.collect(Collectors.<Boolean>toList());
+				
+		return booleanList.toArray(results);
+
 	}
+
 }
